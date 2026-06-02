b4d7edb61f893ac3c515e7d74760c0ead8c721a4
kukucaiCndy <kukucaiCndy@users.noreply.github.com>
2026-06-02 23:05:50 +0800

    refactor: 测试页改为双模式切换 - 空web-view vs 有URL的web-view，右上角按钮切换

--- miniapp/pages/test-coverview/test-coverview.js
+++ miniapp/pages/test-coverview/test-coverview.js
@@ -1,6 +1,21 @@
 Page({
   data: {
-    url: 'http://192.168.16.129:12302'
+    hasUrl: false,
+    url: '',
+    statusBarHeight: 44
+  },
+
+  onLoad() {
+    var sysInfo = wx.getSystemInfoSync();
+    this.setData({ statusBarHeight: sysInfo.statusBarHeight || 44 });
+  },
+
+  onToggle() {
+    if (this.data.hasUrl) {
+      this.setData({ hasUrl: false, url: '' });
+    } else {
+      this.setData({ hasUrl: true, url: 'http://192.168.16.129:12302' });
+    }
   },
 
   onTap1() {
