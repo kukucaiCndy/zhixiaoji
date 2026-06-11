67986869083c67544b8cf90a284a68d02280d667
kukucaiCndy <kukucaiCndy@users.noreply.github.com>
2026-06-11 23:00:29 +0800

    feat: 1:1复刻主页设计稿 (RweGQ)
    - 精确还原 Header、HeroCard、QuickActions、ProgressSection、RecommendSection 尺寸/颜色/间距
    - 添加7个装饰元素 (deco_wave, deco_circle, deco_star, deco_dot, deco_sparkle)
    - 更新 tab-bar 组件样式匹配设计稿 (背景图、阴影、指示器、颜色)
    - 修复成就按钮事件绑定 (onAchievementTap)

--- miniapp/pages/home/home.js
+++ miniapp/pages/home/home.js
@@ -13,7 +13,10 @@ Page({
       accuracy: 0
     },
     progress: [],
-    recommendations: []
+    overallPercent: 0,
+    recommendations: [],
+    continueTitle: '',
+    continueDesc: ''
   },
 
   onLoad() {
@@ -45,7 +48,36 @@ Page({
     });
 
     knowledgeApi.getStudyProgress().then(function (progress) {
-      this.setData({ progress: progress });
+      var overallPercent = 0;
+      var continueTitle = '';
+      var continueDesc = '';
+
+      if (progress && progress.length > 0) {
+        var totalPercent = 0;
+        for (var i = 0; i < progress.length; i++) {
+          totalPercent += (progress[i].percent || 0);
+        }
+        overallPercent = Math.round(totalPercent / progress.length);
+
+        var activeItems = progress.filter(function (item) {
+          return item.percent > 0 && item.percent < 100;
+        });
+        if (activeItems.length > 0) {
+          activeItems.sort(function (a, b) { return b.percent - a.percent; });
+          continueTitle = activeItems[0].name;
+          continueDesc = '已完成 ' + activeItems[0].percent + '%，继续加油';
+        } else if (progress[0]) {
+          continueTitle = progress[0].name;
+          continueDesc = progress[0].percent === 100 ? '已完成，探索新内容吧' : '开始你的第一节课';
+        }
+      }
+
+      this.setData({
+        progress: progress,
+        overallPercent: overallPercent,
+        continueTitle: continueTitle,
+        continueDesc: continueDesc
+      });
     }.bind(this)).catch(function (err) {
       console.error('[Home] Failed to load progress:', err);
     });
@@ -66,22 +98,65 @@ Page({
     });
   },
 
-  onTapCard(e) {
-    var id = e.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: '/pages/learn/learn?cardId=' + id
-    });
+  /* ---- 顶部状态栏 ---- */
+  onProfileTap() {
+    wx.switchTab({ url: '/pages/profile/index' });
   },
 
-  onStartLearn() {
-    wx.switchTab({
-      url: '/pages/learn/learn'
-    });
+  onMallTap() {
+    wx.navigateTo({ url: '/pages/mall/index' });
+  },
+
+  /* ---- 核心CTA ---- */
+  onContinueLearn() {
+    wx.switchTab({ url: '/pages/learn/index' });
+  },
+
+  /* ---- 快捷入口 ---- */
+  onChallenge() {
+    wx.navigateTo({ url: '/pages/learn/daily-challenge/index' });
   },
 
   onStartReview() {
-    wx.switchTab({
-      url: '/pages/learn/learn'
+    wx.navigateTo({ url: '/pages/learn/review/index' });
+  },
+
+  onAchievementTap() {
+    wx.navigateTo({ url: '/pages/achievement/index' });
+  },
+
+  onRankTap() {
+    wx.navigateTo({ url: '/pages/rank/index' });
+  },
+
+  /* ---- 学习进度 ---- */
+  onViewDetail() {
+    wx.switchTab({ url: '/pages/learn/index' });
+  },
+
+  /* ---- 推荐内容 ---- */
+  onMore() {
+    wx.switchTab({ url: '/pages/learn/index' });
+  },
+
+  onRecommendTap(e) {
+    var id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/learn/knowledge-chapters/knowledge-chapters?id=' + id
     });
+  },
+
+  /* ---- Tab栏 ---- */
+  onTabChange(e) {
+    var index = e.detail;
+    var urls = [
+      '/pages/home/home',
+      '/pages/learn/index',
+      '/pages/note/index',
+      '/pages/profile/index'
+    ];
+    if (index >= 0 && index < urls.length) {
+      wx.switchTab({ url: urls[index] });
+    }
   }
 });
